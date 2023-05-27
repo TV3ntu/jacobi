d4c9868b9216c22287f288a54cdebfb50dfe085e
tventu <tomas.venturini@mercadolibre.com>
2023-05-27 18:41:03 -0300

    jacobi

--- jacobi.py
+++ jacobi.py
@@ -1,16 +1,19 @@
 import numpy as np
 
 
-def is_symmetric(matrix):
-    """
-    Verifica si una matriz es simétrica.
-    :param matrix:
-    :return:
-    """
-    return np.allclose(matrix, matrix.T)
+def es_apta_para_jacobi(A):
+    n = A.shape[0]  # Tamaño de la matriz A
+    diagonal = np.abs(A.diagonal())  # Valores absolutos de los elementos de la diagonal
+    resto = np.sum(np.abs(A), axis=1) - diagonal  # Suma de los valores absolutos de los elementos de cada fila, excluyendo la diagonal
+
+    for i in range(n):
+        if diagonal[i] <= resto[i]:
+            return False
+
+    return True
 
 
-def input_symmetric_matrix(n):
+def input_matrix(n):
     """
     Solicita al usuario que ingrese una matriz simétrica.
     :param n:
@@ -21,10 +24,10 @@ def input_symmetric_matrix(n):
         A = np.zeros((n, n))
         for i in range(n):
             A[i] = input().split()
-        if is_symmetric(A):
+        if es_apta_para_jacobi(A):
             return A
         else:
-            print("ERROR: La matriz no es simétrica, intente nuevamente")
+            print("ERROR: La matriz no es apta para jacobi, intente nuevamente")
 
 
 def input_data():
@@ -34,7 +37,7 @@ def input_data():
     """
     n = int(input("Ingrese el valor de n (tamaño de la matriz): "))
 
-    A = input_symmetric_matrix(n)
+    A = input_matrix(n)
 
     print("Ingrese el vector b:")
     b = input().split()
@@ -76,9 +79,23 @@ def jacobi(A, b, k):
         D = np.diag(A)  # Diagonal de A
         R = A - np.diagflat(D)  # Resto de A
 
+        print(f"Iteración {i + 1}:")
+        print("Matriz A:")
+        print(A)
+        print("Matriz diagonal D:")
+        print(D)
+        print("Matriz resto R:")
+        print(R)
+        print("Vector solución x:")
+        print(x)
+
         x = (b - np.dot(R, x)) / D
 
         H[i] = x
+
+        print("Nuevo vector solución x:")
+        print(x)
+        print("--------------------")
 
     return H, np.linalg.norm(H[-1]), x
 
